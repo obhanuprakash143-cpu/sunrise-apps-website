767c95746fad2c9e04bb96bd71a6762ff59968d0
Bhanu Prakash <thesunrisecode@gmail.com>
2026-04-30 16:33:10 +0530

    fixed supabase url and setup policies

--- src/supabaseClient.ts.tsx
+++ src/supabaseClient.ts.tsx
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Replace with your actual details from Supabase
+// Supabase URL ఎప్పుడూ 'https://' తో మొదలవ్వాలి
 const supabaseUrl = 'https://onczdkiimtfpvohrpcxa.supabase.co'
+
+// నీ Anon Key కరెక్ట్‌గానే ఉంది
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uY3pka2lpbXRmcHZvaHJwY3hhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczOTQwMDUsImV4cCI6MjA5Mjk3MDAwNX0.1_tll-fX9DvrDiOEisWmz0VOt2qnFMlqYO0DRvv_Kv8'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
